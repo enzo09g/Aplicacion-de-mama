62aa11617650dff6456be145ae67dfadaf2dcfbb
Enzo Gómez <enzoeegv@gmail.com>
2023-09-13 05:29:31 -0300

    Funcionalidades borrar, agregar y cargar actualiza

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
+let arrayClientes = [];
 
 function agregar(array) {
-
 
   if (checkEnviar() && checkIdentico(array)) {
     let cliente = {};
@@ -12,8 +11,22 @@ function agregar(array) {
     cliente.direccion = document.getElementById("inputDireccion").value;
     array.push(cliente);
     vaciarInputs();
+    enviarLista(array)
+    actualizarArrayClientes();
     mostrar(cliente, array);
   }
+}
+
+function actualizarArrayClientes(){
+  arrayClientes = JSON.parse(localStorage.getItem('lista') || []);
+}
+
+function enviarLista(array){
+  localStorage.setItem("lista", JSON.stringify(array));
+}
+
+function obtenerUltimoIndice(array) {
+  return array.length - 1;
 }
 
 function vaciarInputs() {
@@ -57,35 +70,43 @@ function mostrar(objeto, arrayClientes) {
             <td>${objeto.cedula}</td>
             <td>${objeto.numero}</td>
             <td>${objeto.direccion}</td>
-            <td><button class="eliminar btn btn-danger"></td>
-
+            <td><button onclick="borrarElemento(${obtenerUltimoIndice(arrayClientes)})" id="${obtenerUltimoIndice(arrayClientes)}" class="eliminar btn btn-danger"></td>
         </tr>
     `;
-  localStorage.setItem("lista", JSON.stringify(arrayClientes));
+
 }
 
-function cargarLista() {
+function cargarLista(lista) {
   let contenedor = document.getElementById("contenedor-lista");
-  let lista = JSON.parse(localStorage.getItem("lista")) || [];
-  for (let i of lista) {
+  console.log(typeof lista)
+  for (let i = 0; i < lista.length; i++) {
     contenedor.innerHTML += `
             <tr>
-                <td>${i.nombre}</td>
-                <td>${i.apellido}</td>
-                <td>${i.cedula}</td>
-                <td>${i.numero}</td>
-                <td>${i.direccion}</td>
-                <td><button class="eliminar btn btn-danger"></td>
-
+                <td>${lista[i].nombre}</td>
+                <td>${lista[i].apellido}</td>
+                <td>${lista[i].cedula}</td>
+                <td>${lista[i].numero}</td>
+                <td>${lista[i].direccion}</td>
+                <td><button onclick="borrarElemento(${i})" id="${i}" class="eliminar btn btn-danger"></button></td>
             </tr>
         `;
   }
 }
 
+function borrarElemento(indice) {
+
+  actualizarArrayClientes();
+  arrayClientes.splice(indice, 1);
+  enviarLista(arrayClientes);
+  document.getElementById('contenedor-lista').textContent = ""
+  actualizarArrayClientes();
+  cargarLista(arrayClientes);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  let arrayClientes = JSON.parse(localStorage.getItem("lista")) || [];
-  cargarLista();
+  actualizarArrayClientes();
   let btnGuardar = document.getElementById("btnGuardar");
+  cargarLista(arrayClientes);
   btnGuardar.addEventListener("click", () => {
     agregar(arrayClientes);
   });
