d10a34116eabb7a14d4796b25b644f6b16500489
Enzo Gómez <enzoeegv@gmail.com>
2023-09-13 00:07:20 -0300

    Agregar y cargar listo

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let arrayClientes = [];
+function agregar(array) {
+  let inputs = document.getElementsByTagName("input");
+  console.log(inputs.length);
 
-function agregar(){
-
-    let inputs = document.getElementsByTagName('input');
-    console.log(inputs.length());
-
-    let cliente = {}; 
-    cliente.nombre = document.getElementById('inputNombre').value;
-    cliente.apellido = document.getElementById('inputApellido').value;
-    cliente.cedula = document.getElementById('inputCedula').value;
-    cliente.numero = document.getElementById('inputNumero').value;
-    cliente.direccion = document.getElementById('inputDireccion').value;
-    arrayClientes.push(cliente);
-    mostrar(cliente)
+  let cliente = {};
+  cliente.nombre = document.getElementById("inputNombre").value;
+  cliente.apellido = document.getElementById("inputApellido").value;
+  cliente.cedula = document.getElementById("inputCedula").value;
+  cliente.numero = document.getElementById("inputNumero").value;
+  cliente.direccion = document.getElementById("inputDireccion").value;
+  array.push(cliente);
+  mostrar(cliente, array);
 }
 
-function mostrar(objeto){
-    let contenedor = document.getElementById('contenedor-lista');
-    contenedor.innerHTML += `
+function mostrar(objeto, arrayClientes) {
+  let contenedor = document.getElementById("contenedor-lista");
+  contenedor.innerHTML += `
         <tr>
             <td>${objeto.nombre}</td>
             <td>${objeto.apellido}</td>
@@ -25,15 +22,15 @@ function mostrar(objeto){
             <td>${objeto.numero}</td>
             <td>${objeto.direccion}</td>
         </tr>
-    `
-    localStorage.setItem('lista', JSON.stringify(arrayClientes));
+    `;
+  localStorage.setItem("lista", JSON.stringify(arrayClientes));
 }
 
-function cargarLista(){
-    let contenedor = document.getElementById('contenedor-lista');
-    let lista = JSON.parse(localStorage.getItem('lista'));
-    for(let i of lista){
-        contenedor.innerHTML += `
+function cargarLista() {
+  let contenedor = document.getElementById("contenedor-lista");
+  let lista = JSON.parse(localStorage.getItem("lista")) || [];
+  for (let i of lista) {
+    contenedor.innerHTML += `
             <tr>
                 <td>${i.nombre}</td>
                 <td>${i.apellido}</td>
@@ -41,14 +38,15 @@ function cargarLista(){
                 <td>${i.numero}</td>
                 <td>${i.direccion}</td>
             </tr>
-        `
-    }
+        `;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () =>{
-    cargarLista();
-    let btnGuardar = document.getElementById('btnGuardar');
-    btnGuardar.addEventListener('click', () =>{
-        agregar();
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  let arrayClientes = JSON.parse(localStorage.getItem("lista")) || [];
+  cargarLista();
+  let btnGuardar = document.getElementById("btnGuardar");
+  btnGuardar.addEventListener("click", () => {
+    agregar(arrayClientes);
+  });
+});
